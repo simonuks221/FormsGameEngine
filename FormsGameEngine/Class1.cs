4e9ae8e53f442013532c0c47d96dffdd4698fd93
simonuks221 <simonasriauka@gmail.com>
2019-05-24 21:14:57 +0300

    Start

--- FormsGameEngine/Class1.cs
+++ FormsGameEngine/Class1.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace FormsGameEngine
 {
-    public class Class1
+    public class MainGameEnginePanel : Panel
     {
+        public MainGameEnginePanel(Size _panelSize)
+        {
+            this.Size = _panelSize;
+            this.BackColor = Color.Aqua;
+        }
+
     }
 }
---
+++ FormsGameEngineFormExample/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FormsGameEngine;
+
+namespace FormsGameEngineFormExample
+{
+    public partial class Form1 : Form
+    {
+        MainGameEnginePanel mainGameEnginePanel;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mainGameEnginePanel = new MainGameEnginePanel(new Size(700, 400));
+            Controls.Add(mainGameEnginePanel);
+            mainGameEnginePanel.Location = new Point(0, 0);
+        }
+    }
+}
